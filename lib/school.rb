d1dc48d824f4e2aabc80097b4202d29c025a35bf
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-15 03:42:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,15 @@
 class School 
   def school=(school)
     @school = school 
+  end 
     
   def school 
     @school 
+  end 
+  
+  def roster 
+    roster = {}
+  end 
+  
+  def add_student 
+  end 
